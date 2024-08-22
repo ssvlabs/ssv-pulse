e95bbb13b776b036585f9e69b0774a4d1b703094
oleg-ssvlabs <oleg.iar@ssvlabs.io>
2024-08-22 19:55:26 +0200

    Add metric for unready blocks

--- internal/benchmark/metrics/consensus/attestation.go
+++ internal/benchmark/metrics/consensus/attestation.go
@@ -18,10 +18,15 @@ import (
 
 const (
 	blockMintingTime             = time.Second * 12
+	unreadyBlockDelayMs          = 200
 	MissedBlockMeasurement       = "MissedBlock"
 	ReceivedBlockMeasurement     = "ReceivedBlock"
 	MissedAttestationMeasurement = "MissedAttestation"
 	FreshAttestationMeasurement  = "FreshAttestation"
+)
+
+var (
+	UnreadyBlockMeasurement = fmt.Sprintf("UnreadyBlockMeasurement%dms", unreadyBlockDelayMs)
 )
 
 type (
@@ -67,8 +72,9 @@ func (a *AttestationMetric) Measure() {
 	if a.isLaunched {
 		return
 	}
+	ctx := context.TODO()
 
-	go a.launchListener()
+	go a.launchListener(ctx)
 
 	go func() {
 		slot := currentSlot(a.genesisTime)
@@ -80,7 +86,7 @@ func (a *AttestationMetric) Measure() {
 			select {
 			case <-nextSlotWithDelay:
 				go func(slot phase0.Slot) {
-					a.fetchAttestationData(slot)
+					a.fetchAttestationData(ctx, slot)
 
 					if slot > laggedSlot {
 						a.calculateMeasurements(slot - calculationSlotLag)
@@ -95,9 +101,56 @@ func (a *AttestationMetric) Measure() {
 	a.isLaunched = true
 }
 
-func (a *AttestationMetric) fetchAttestationData(slot phase0.Slot) {
-	attestationData, err := a.client.(client.AttestationDataProvider).AttestationData(
-		context.TODO(),
+func (a *AttestationMetric) fetchAttestationData(ctx context.Context, slot phase0.Slot) {
+	blockRoot, err := a.fetchAttestationBlockRoot(ctx, slot)
+
+	if err != nil {
+		logger.WriteError(metric.ConsensusGroup, a.Name, err)
+		return
+	}
+	a.attestationBlockRoots.Store(slot, blockRoot)
+}
+
+func (a *AttestationMetric) launchListener(ctx context.Context) {
+	if err := a.client.(client.EventsProvider).Events(
+		ctx,
+		[]string{"head"},
+		func(event *v1.Event) {
+			data := event.Data.(*v1.HeadEvent)
+
+			a.eventBlockRoots.Store(data.Slot, SlotData{
+				Received:  time.Now(),
+				RootBlock: data.Block,
+			})
+
+			go a.checkUnreadyBlock(data.Slot, data.Block)
+		},
+	); err != nil {
+		logger.WriteError(metric.ConsensusGroup, a.Name, err)
+	}
+}
+
+func (a *AttestationMetric) checkUnreadyBlock(slot phase0.Slot, block phase0.Root) {
+	time.Sleep(time.Millisecond * unreadyBlockDelayMs)
+	blockRoot, err := a.fetchAttestationBlockRoot(context.TODO(), slot)
+	if err != nil {
+		logger.WriteError(metric.ConsensusGroup, a.Name, err)
+		return
+	}
+
+	if blockRoot != block {
+		a.AddDataPoint(map[string]float64{
+			UnreadyBlockMeasurement: 1,
+		})
+		logger.WriteMetric(metric.ConsensusGroup, a.Name, map[string]any{
+			UnreadyBlockMeasurement: 1,
+		})
+	}
+}
+
+func (a *AttestationMetric) fetchAttestationBlockRoot(ctx context.Context, slot phase0.Slot) (phase0.Root, error) {
+	resp, err := a.client.(client.AttestationDataProvider).AttestationData(
+		ctx,
 		&api.AttestationDataOpts{
 			Slot:           slot,
 			CommitteeIndex: 0,
@@ -105,49 +158,36 @@ func (a *AttestationMetric) fetchAttestationData(slot phase0.Slot) {
 		},
 	)
 	if err != nil {
-		logger.WriteError(metric.ConsensusGroup, a.Name, err)
-		return
+		return phase0.Root{}, err
 	}
-	a.attestationBlockRoots.Store(slot, attestationData.Data.BeaconBlockRoot)
-}
 
-func (a *AttestationMetric) launchListener() {
-	if err := a.client.(client.EventsProvider).Events(
-		context.TODO(),
-		[]string{"head"},
-		func(event *v1.Event) {
-			data := event.Data.(*v1.HeadEvent)
-			a.eventBlockRoots.Store(data.Slot, SlotData{
-				Received:  time.Now(),
-				RootBlock: data.Block,
-			})
-		},
-	); err != nil {
-		logger.WriteError(metric.ConsensusGroup, a.Name, err)
-	}
+	return resp.Data.BeaconBlockRoot, nil
 }
 
 func (a *AttestationMetric) AggregateResults() string {
 	close(a.quitChan)
-	var missedAttestations, freshAttestations, missedBlocks, receivedBlocks float64
+	var missedAttestations, freshAttestations, missedBlocks, receivedBlocks, unreadyBlocks float64
 
 	for _, point := range a.DataPoints {
 		missedAttestations += point.Values[MissedAttestationMeasurement]
 		missedBlocks += point.Values[MissedBlockMeasurement]
 		freshAttestations += point.Values[FreshAttestationMeasurement]
 		receivedBlocks += point.Values[ReceivedBlockMeasurement]
+		unreadyBlocks += point.Values[UnreadyBlockMeasurement]
 	}
 
-	return fmt.Sprintf("missed_attestations=%.0f, fresh_attestations=%.0f, missed_blocks=%.0f \n received_blocks=%.0f, correctness=%.2f %%",
+	return fmt.Sprintf(
+		"missed_attestations=%.0f, unready_blocks_%d_ms=%.0f, missed_blocks=%.0f \n fresh_attestations=%.0f received_blocks=%.0f, correctness=%.2f %%",
 		missedAttestations,
-		freshAttestations,
+		unreadyBlockDelayMs, unreadyBlocks,
 		missedBlocks,
+		freshAttestations,
 		receivedBlocks,
 		freshAttestations/receivedBlocks*100)
 }
 
 func (a *AttestationMetric) calculateMeasurements(slot phase0.Slot) {
-	receival, ok := a.eventBlockRoots.Load(slot)
+	eventBlockRoot, ok := a.eventBlockRoots.Load(slot)
 
 	if !ok {
 		a.AddDataPoint(map[string]float64{
@@ -167,7 +207,7 @@ func (a *AttestationMetric) calculateMeasurements(slot phase0.Slot) {
 		ReceivedBlockMeasurement: 1,
 	})
 
-	blockRoot, ok := a.attestationBlockRoots.Load(slot)
+	attestationBlockRoot, ok := a.attestationBlockRoots.Load(slot)
 	if !ok {
 		a.AddDataPoint(map[string]float64{
 			MissedAttestationMeasurement: 1,
@@ -179,7 +219,7 @@ func (a *AttestationMetric) calculateMeasurements(slot phase0.Slot) {
 		return
 	}
 
-	if blockRoot == receival.(SlotData).RootBlock {
+	if attestationBlockRoot == eventBlockRoot.(SlotData).RootBlock {
 		a.AddDataPoint(map[string]float64{
 			FreshAttestationMeasurement: 1,
 		})
