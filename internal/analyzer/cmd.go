37db00f8cf6b976b5577001c941e5fc5a65921a9
oleg-ssvlabs <oleg.iar@ssvlabs.io>
2024-09-18 10:12:04 +0200

    Refactor Analyzer cmd with tests

--- internal/analyzer/cmd.go
+++ internal/analyzer/cmd.go
@@ -1,48 +1,37 @@
 package analyzer
 
 import (
-	"fmt"
-	"strings"
-
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
 	"github.com/ssvlabsinfra/ssv-pulse/configs"
-	"github.com/ssvlabsinfra/ssv-pulse/internal/platform/cmd"
 )
 
-const filePathFlag = "log-file-path"
+const (
+	logFilePathFlag = "log-file-path"
+	operatorsFlag   = "operators"
+	clusterFlag     = "cluster"
+
+	command = "analyzer"
+)
 
 func init() {
-	cmd.AddPersistentStringFlag(CMD, "logFilePath", "", "Path to ssv node log file to analyze", true)
-	cmd.AddPersistentStringSliceFlag(CMD, "operators", []string{}, "Operators to analyze", false)
-	cmd.AddPersistentBoolFlag(CMD, "cluster", false, "Are operators forming the cluster?", false)
+	addFlags(CMD)
+	if err := bindFlags(CMD); err != nil {
+		panic(err.Error())
+	}
 }
 
 var CMD = &cobra.Command{
-	Use:   "log-analyzer",
+	Use:   command,
 	Short: "Read and analyze ssv node logs",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		isValid, err := configs.Values.Analyzer.Validate()
 		if !isValid {
-			panic(err.Error())
-		}
-		logFilePath := viper.GetString("logFilePath")
-		if strings.Contains(logFilePath, "../") {
-			return fmt.Errorf("❕ flag should not contain traversal")
-		}
-		if logFilePath == "" {
-			return fmt.Errorf("❕ logFilePath flag can not be empty")
-		}
-		if err := viper.BindPFlag("cluster", cmd.PersistentFlags().Lookup("cluster")); err != nil {
 			return err
 		}
-		cluster := viper.GetBool("cluster")
-		if err := viper.BindPFlag("operators", cmd.PersistentFlags().Lookup("operators")); err != nil {
-			return err
-		}
-		operators := viper.GetStringSlice("operators")
-		analyzer, err := New(logFilePath, operators, cluster)
+
+		analyzer, err := New(configs.Values.Analyzer.LogFilePath, configs.Values.Analyzer.Operators, configs.Values.Analyzer.Cluster)
 		if err != nil {
 			return err
 		}
@@ -52,4 +41,24 @@ var CMD = &cobra.Command{
 		}
 		return nil
 	},
+}
+
+func addFlags(cobraCMD *cobra.Command) {
+	cobraCMD.Flags().String(logFilePathFlag, "", "Path to ssv node log file to analyze")
+	cobraCMD.Flags().StringSlice(operatorsFlag, []string{}, "Operators to analyze")
+	cobraCMD.Flags().Bool(clusterFlag, false, "Are operators forming the cluster?")
+}
+
+func bindFlags(cmd *cobra.Command) error {
+	if err := viper.BindPFlag("analyzer.log-file-path", cmd.Flags().Lookup(logFilePathFlag)); err != nil {
+		return err
+	}
+	if err := viper.BindPFlag("analyzer.cluster", cmd.Flags().Lookup(clusterFlag)); err != nil {
+		return err
+	}
+	if err := viper.BindPFlag("analyzer.operators", cmd.Flags().Lookup(operatorsFlag)); err != nil {
+		return err
+	}
+
+	return nil
 }
--- internal/analyzer/log_analyzer_test.go
+++ internal/analyzer/log_analyzer_test.go
@@ -1,52 +1,49 @@
 package analyzer
 
 import (
-	"os"
+	"fmt"
 	"reflect"
 	"testing"
 	"unsafe"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 	"github.com/stretchr/testify/require"
 
-	"github.com/ssvlabsinfra/ssv-pulse/internal/platform/cmd"
+	"github.com/ssvlabsinfra/ssv-pulse/configs"
 )
 
 func TestProposeAnalyze(t *testing.T) {
-	err := os.RemoveAll("./output/")
-	require.NoError(t, err)
-	version := "test.version"
-	RootCmd := &cobra.Command{
-		Use:   "log-analyze",
-		Short: "Read and analyze ssv node logs",
+	const testLogFilePath = "./test/propose_test.log"
+	rootCMD := &cobra.Command{
 		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			err := viper.Unmarshal(&configs.Values)
+			require.NoError(t, err)
 		},
 	}
-	RootCmd.AddCommand(CMD)
-	RootCmd.AddCommand(cmd.Version)
-	RootCmd.Short = "log-analyze"
-	RootCmd.Version = version
+	rootCMD.AddCommand(CMD)
+
 	t.Run("test propose logs - cluster", func(t *testing.T) {
-		args := []string{"log-analyzer", "--logFilePath", "./test/propose_test.log", "--operators", "54,178,225,226,227,228,229", "--cluster"}
-		RootCmd.SetArgs(args)
-		err := RootCmd.Execute()
+		args := []string{command, fmt.Sprintf("--%s", logFilePathFlag), testLogFilePath, fmt.Sprintf("--%s", operatorsFlag), "54,178,225,226,227,228,229", fmt.Sprintf("--%s", clusterFlag)}
+		rootCMD.SetArgs(args)
+		err := rootCMD.Execute()
 		require.NoError(t, err)
-		resetFlags(RootCmd)
+		resetFlags(rootCMD)
 	})
 	t.Run("test propose logs - not cluster", func(t *testing.T) {
-		args := []string{"log-analyzer", "--logFilePath", "./test/propose_test.log", "--operators", "54,178,225,226,227,228,229"}
-		RootCmd.SetArgs(args)
-		err := RootCmd.Execute()
+		args := []string{command, fmt.Sprintf("--%s", logFilePathFlag), testLogFilePath, fmt.Sprintf("--%s", operatorsFlag), "54,178,225,226,227,228,229"}
+		rootCMD.SetArgs(args)
+		err := rootCMD.Execute()
 		require.NoError(t, err)
-		resetFlags(RootCmd)
+		resetFlags(rootCMD)
 	})
 	t.Run("test propose logs - all", func(t *testing.T) {
-		args := []string{"log-analyzer", "--logFilePath", "./test/propose_test.log"}
-		RootCmd.SetArgs(args)
-		err := RootCmd.Execute()
+		args := []string{command, fmt.Sprintf("--%s", logFilePathFlag), testLogFilePath}
+		rootCMD.SetArgs(args)
+		err := rootCMD.Execute()
 		require.NoError(t, err)
-		resetFlags(RootCmd)
+		resetFlags(rootCMD)
 	})
 }
 
