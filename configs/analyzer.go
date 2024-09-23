33f8f08819e23deda72605ee872144b9dbf39bae
oleg-ssvlabs <oleg.iar@ssvlabs.io>
2024-09-23 16:17:10 +0200

    Add cluster size validation

--- configs/analyzer.go
+++ configs/analyzer.go
@@ -2,6 +2,7 @@ package configs
 
 import (
 	"errors"
+	"fmt"
 	"strings"
 )
 
@@ -23,9 +24,23 @@ func (a Analyzer) Validate() (bool, error) {
 		return false, errors.New("if cluster is set to 'true', the list of operators cannot be empty")
 	}
 
+	if a.Cluster {
+		if !isValidClusterSize(a.Operators) {
+			return false, fmt.Errorf("the cluster size: '%d' is not valid'", len(a.Operators))
+		}
+	}
+
 	return true, nil
 }
 
 func (a Analyzer) WithScores() bool {
 	return len(a.Operators) != 0 && a.Cluster
+}
+
+func isValidClusterSize(operators []uint32) bool {
+	if len(operators) < 4 || len(operators) > 13 || len(operators)%2 != 1 {
+		return false
+	}
+
+	return true
 }
