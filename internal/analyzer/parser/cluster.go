73990f0346665a860bb34ab6c7847d32e81a7cf3
oleg-ssvlabs <oleg.iar@ssvlabs.io>
2024-09-23 16:32:47 +0200

    Remove redundand operator length check

--- internal/analyzer/parser/cluster.go
+++ internal/analyzer/parser/cluster.go
@@ -3,10 +3,6 @@ package parser
 import "time"
 
 func IsCluster(operators []SignerID, signers map[SignerID]time.Time) bool {
-	if len(operators) < 4 || len(operators) > 13 || len(operators)%2 != 1 {
-		return false
-	}
-
 	if len(operators) != len(signers) {
 		return false
 	}
