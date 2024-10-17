40ad44b607015cc07bc8cc789118293579e01b9b
oleg-ssvlabs <oleg.iar@ssvlabs.io>
2024-10-17 09:31:35 +0200

    Refactor: move array util function to a different directory

--- internal/platform/array/array.go
+++ internal/platform/array/array.go
@@ -1,4 +1,4 @@
-package utils
+package array
 
 import (
 	"fmt"
@@ -27,4 +27,41 @@ func StingSliceToUintArray(flagdata []string) ([]uint64, error) {
 		return nil, fmt.Errorf("slice isnt sorted")
 	}
 	return partsarr, nil
+}
+
+func CollectDistinct[T comparable](arrays ...[]T) []T {
+	uniqueValues := make(map[T]struct{})
+
+	for _, array := range arrays {
+		for _, value := range array {
+			uniqueValues[value] = struct{}{}
+		}
+	}
+
+	result := make([]T, 0, len(uniqueValues))
+	for value := range uniqueValues {
+		result = append(result, value)
+	}
+
+	return result
+}
+
+func SameMembers[T comparable](arr1, arr2 []T) bool {
+	if len(arr1) != len(arr2) {
+		return false
+	}
+
+	elementCount := make(map[T]int)
+	for _, elem := range arr1 {
+		elementCount[elem]++
+	}
+
+	for _, elem := range arr2 {
+		if count, found := elementCount[elem]; !found || count == 0 {
+			return false
+		}
+		elementCount[elem]--
+	}
+
+	return true
 }
