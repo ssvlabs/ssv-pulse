5549acf84a03644877b1eae96a4f66c26a9ceaed
oleg-ssvlabs <oleg.iar@ssvlabs.io>
2025-03-24 12:00:00 +0100

    Add support for Hoodi network

--- internal/benchmark/metric.go
+++ internal/benchmark/metric.go
@@ -67,7 +67,7 @@ func LoadEnabledMetrics(config configs.Config) (map[metric.Group][]metricService
 				consensus.NewAttestationMetric(
 					addr,
 					"Attestation",
-					network.GenesisTime[network.Name(config.Benchmark.Network)],
+					network.Supported[network.Name(config.Benchmark.Network)].GenesisTime,
 					[]metric.HealthCondition[float64]{
 						{Name: consensus.CorrectnessMeasurement, Threshold: 97, Operator: metric.OperatorLessThanOrEqual, Severity: metric.SeverityHigh},
 						{Name: consensus.CorrectnessMeasurement, Threshold: 98.5, Operator: metric.OperatorLessThanOrEqual, Severity: metric.SeverityMedium},
--- internal/platform/network/network.go
+++ internal/platform/network/network.go
@@ -2,27 +2,39 @@ package network
 
 import (
 	"fmt"
+	"maps"
+	"slices"
 	"strings"
 	"time"
 )
 
-var (
-	GenesisTime = map[Name]time.Time{
-		Holesky: time.Unix(1695902400, 0),
-		Mainnet: time.Unix(1606824023, 0),
+type (
+	Name    string
+	Network struct {
+		Name        Name
+		GenesisTime time.Time
 	}
 )
-
-type Name string
 
 const (
 	Holesky Name = "holesky"
 	Mainnet Name = "mainnet"
+	Hoodi   Name = "hoodi"
+)
+
+var (
+	Supported = map[Name]Network{
+		Holesky: {Name: Holesky, GenesisTime: time.Unix(1695902400, 0)},
+		Mainnet: {Name: Mainnet, GenesisTime: time.Unix(1606824023, 0)},
+		Hoodi:   {Name: Hoodi, GenesisTime: time.Unix(1742213400, 0)},
+	}
 )
 
 func (n Name) Validate() error {
-	if !strings.EqualFold(string(n), string(Holesky)) && !strings.EqualFold(string(n), string(Mainnet)) {
-		return fmt.Errorf("network name should be either '%s' or '%s'", Holesky, Mainnet)
+	_, ok := Supported[Name(strings.ToLower(string(n)))]
+	if !ok {
+		return fmt.Errorf("unsupported network name: '%s'. List of supported networks: '%v'", n, slices.Collect(maps.Keys(Supported)))
 	}
+
 	return nil
 }
